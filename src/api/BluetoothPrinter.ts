dd8b34bb29a28c29b2d28e912a5e1f8b417da7f3
Sharmila Konapala <sharmilakonapala@Sharmilas-MacBook-Air.local>
2026-04-16 14:47:07 +0530

    Technology: Updated official support contact number on tickets

--- src/api/BluetoothPrinter.ts
+++ src/api/BluetoothPrinter.ts
@@ -191,7 +191,7 @@ class BluetoothPrinterService {
             // 7. Footer
             await this.write(this.COMMANDS.ALIGN_CENTER);
             await this.write(encoder.encode("\nWWW.ETHREE.IN\n"));
-            await this.write(encoder.encode("Support: +91 88864 71111\n"));
+            await this.write(encoder.encode("Support: +91 70369 23456\n"));
             await this.write(encoder.encode("Thank You! Visit Again\n"));
             
             // 7. Cut
